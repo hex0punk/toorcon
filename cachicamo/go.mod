f771f7fbe7e35a6bc0820702e52e0592adb81a92
hex0punk <alex.useche@trailofbits.com>
2021-10-13 08:50:50 -0700

    another go.mod update

--- cachicamo/go.mod
+++ cachicamo/go.mod
@@ -1,4 +1,4 @@
-module cachicamo
+module toorcon/cachicamo
 
 go 1.13
 
--- cachicamo/main.go
+++ cachicamo/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"cachicamo/helpers"
-	"cachicamo/visitors"
+	"toorcon/cachicamo/helpers"
+	"toorcon/cachicamo/visitors"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"log"
